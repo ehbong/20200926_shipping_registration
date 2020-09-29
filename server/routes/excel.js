b80916b44e6127f65d1d93d989a993451cf2c719
ehbong <hangillw86@gmail.com>
2020-09-29 18:35:19 +0900

    add comment

--- server/routes/excel.js
+++ server/routes/excel.js
@@ -17,11 +17,13 @@ router.post("", (req, res) => {
   });
 
   form.on("file", (name, file) => {
+    // 파일 불러오기
     const workbook = xlsx.readFile(file.path);
+    // 시트 이름을 키 배열로 추출
     const sheetnames = Object.keys(workbook.Sheets);
 
     let i = sheetnames.length;
-
+    // 반복문을 통해서 각 시트의 데이터를 json데이터로 resData에 담음
     while (i--) {
       const sheetname = sheetnames[i];
       resData[sheetname] = xlsx.utils.sheet_to_json(workbook.Sheets[sheetname]);
